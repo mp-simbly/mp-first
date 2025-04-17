29d71648bf2dea5727af0ff1d179f5071df52120
mp-simbly <michael@simbly.ai>
2025-04-17 18:38:50 +0200

    Linking DB to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,25 @@
-import Link from "next/link";
 import { getDb } from "~/server/db";
-import { posts } from "~/server/db/schema";
+import { desc } from "drizzle-orm";
+
 
 export const dynamic = 'force-dynamic';
 
-const mockURLs = [
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20WwTN5zP3tN1rKkRCsf6pOMLnYUAB5I9ybuZjX",
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20WsUlwL7CRlFUc05my3a249iWCuj8DAHxGNMbZ",
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20WagAPd13zdmRY7jUklMuTfcrq9woHh18P0bx6",
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20W5DEtXS0QFuwZRLV6GrOpAE4IaSqUeB3mkY7T",
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20Wp5WHjsNvqVFM1adJpTs8Q74lNnf3zIBcAiXZ",
-  "https://5h6ewqp3d1.ufs.sh/f/7UfRj4xPm20W6un3voasrQymgZH3fCpXadWclD9YjFPtoKEq"
-]
-
-const mockImages = mockURLs.map((url, index) => ({
-    id: index + 1,
-    url,
-  }))
-
-type Post = typeof posts.$inferSelect;
-
-
 export default async function HomePage() {
-  let posts: Post[] = [];
-  
-  try {
-    console.log('Getting database connection...');
     const db = getDb();
-    console.log('Fetching posts...');
-    posts = await db.query.posts.findMany() as Post[];
-    console.log('Fetched posts:', posts.length, 'records');
-  } catch (error) {
-    console.error('Database query failed:', error);
-  }
+    const images = await db.query.images.findMany({
+      orderBy: (model: typeof images, { desc }: { desc: (column: any) => any }) => [desc(model.id)],
+    });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.length === 0 ? (
-          <div>No posts found in database</div>
-        ) : (
-          posts.map((post) => (
-            <div key={post.id} className="w-48">
-              {post.name}
-            </div>
-          ))
-        )}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex-col">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
-      Hello world
     </main>
   );
 }
