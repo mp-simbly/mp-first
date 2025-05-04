eb390f6bb9c67747c16c0d0902207e5e5f7a9895
mp-simbly <michael@simbly.ai>
2025-05-04 18:27:38 +0200

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import { getDb } from "~/server/db";
 import { desc } from "drizzle-orm";
+import { SignedOut, SignedIn } from "@clerk/nextjs";
+
 
 
 export const dynamic = 'force-dynamic';
+
+async function Images() {
+  const db = getDb();
+  const images = await db.query.images.findMany({
+    orderBy: (model: typeof images, { desc }: { desc: (column: any) => any }) => [desc(model.id)],
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+        {[...images, ...images, ...images].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48 flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
+          </div>
+        ))}
+      </div>
+  )
+}
 
 export default async function HomePage() {
     const db = getDb();
@@ -12,14 +31,13 @@ export default async function HomePage() {
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images/>
+      </SignedIn>
+
     </main>
   );
 }
